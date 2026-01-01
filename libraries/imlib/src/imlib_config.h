caa18ee21f1adadc57efae71fda323a93013edbc
xelll <xel1995@163.com>
2026-01-01 11:27:54 +0800

    feat: enable multiple IMLIB modules in configuration

--- libraries/imlib/src/imlib_config.h
+++ libraries/imlib/src/imlib_config.h
@@ -1,12 +1,5 @@
 #pragma once
 
-// Attempt to include the header file
-#ifdef __has_include
-#if __has_include("imlib_config_user.h")
-#include "imlib_config_user.h"
-#endif
-#endif
-
 #undef OMV_JPEG_CODEC_ENABLE
 #define OMV_JPEG_CODEC_ENABLE (0)
 
@@ -23,31 +16,38 @@
 // #define IMLIB_ENABLE_IMAGE_FILE_IO (0)
 
 // enable imlib_histeq
-// #define IMLIB_ENABLE_HISTEQ
+#define IMLIB_ENABLE_HISTEQ
 
 // enable imlib_mean_filter
-// #define IMLIB_ENABLE_MEAN
+#define IMLIB_ENABLE_MEAN
 
 // enable imlib_median_filter
-// #define IMLIB_ENABLE_MEDIAN
+#define IMLIB_ENABLE_MEDIAN
 
 // enable imlib_mode_filter
-// #define IMLIB_ENABLE_MODE
+#define IMLIB_ENABLE_MODE
 
 // enable imlib_midpoint_filter
-// #define IMLIB_ENABLE_MIDPOINT
+#define IMLIB_ENABLE_MIDPOINT
 
 // enable imlib_morph
-// #define IMLIB_ENABLE_MORPH
+#define IMLIB_ENABLE_MORPH
 
 // enable imlib_bilateral_filter
-// #define IMLIB_ENABLE_BILATERAL
+#define IMLIB_ENABLE_BILATERAL
 
 // enable imlib_find_lines
-// #define IMLIB_ENABLE_FIND_LINES
+#define IMLIB_ENABLE_FIND_LINES
 
 // enable imlib_find_line_segments
-// #define IMLIB_ENABLE_FIND_LINE_SEGMENTS
+#define IMLIB_ENABLE_FIND_LINE_SEGMENTS
 
 // enable imlib_find_circles
 #define IMLIB_ENABLE_FIND_CIRCLES
+
+// Attempt to include the header file
+#ifdef __has_include
+#if __has_include("imlib_config_user.h")
+#include "imlib_config_user.h"
+#endif
+#endif
